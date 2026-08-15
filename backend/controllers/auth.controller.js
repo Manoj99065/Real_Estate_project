55f52ace6515e3c54039926c26f0941c129ed3b7
Manoj <your-email@gmail.com>
2026-08-15 22:50:32 +0530

    hdfskdsk

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,6 +1,6 @@
 import User from '../models/user.model.js';
 import bcrypt from 'bcryptjs';
-import { sendEmail } from '../utils/sendEmail.js';
+import { sendEmail } from '../utils/SendEmail.js';
 import jwt from 'jsonwebtoken';
 import crypto from 'crypto';
 
